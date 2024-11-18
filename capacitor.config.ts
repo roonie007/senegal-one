a0900a71227e354fbeae6bb0b40f296da5d8b6ef
Mouadh HSOUMI <hsmouadh@gmail.com>
2024-11-18 21:08:26 +0100

    feat: add splash screen and icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Sénégal one',
-  webDir: 'dist'
+  appId: "com.cometrip.senegal-one",
+  appName: "Sénégal one",
+  webDir: "dist",
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import { SplashScreen } from "@capacitor/splash-screen";
+
+await SplashScreen.show({
+  showDuration: 2000,
+  autoHide: true,
+});
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
