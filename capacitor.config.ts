d405a4652240a5a523e2e0e1fe533e2f080d5aad
Mouadh HSOUMI <hsmouadh@gmail.com>
2024-11-18 21:26:14 +0100

    feat(android): add night mode splash screens and icon assets, update config with splash screen settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,16 @@ const config: CapacitorConfig = {
   appId: "com.cometrip.senegal-one",
   appName: "Sénégal one",
   webDir: "dist",
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      androidSplashResourceName: "splash",
+      splashFullScreen: true,
+      splashImmersive: true,
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
