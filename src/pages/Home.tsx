474ce84b91067bf5f02e8894e44989791f3f6196
Mouadh HSOUMI <hsmouadh@gmail.com>
2024-11-19 10:23:10 +0100

    feat(Home): add clickable actor and tourist images with navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,35 +10,51 @@ import "./Home.css";
 import { Button } from "@nextui-org/react";
 import { useHistory } from "react-router-dom";
 
+import acteur from "../assets/acteur-2.jpg";
+import touriste from "../assets/touriste-2.jpg";
+
 const Home: React.FC = () => {
   const history = useHistory();
+
+  const goToStays = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    history.push("/stays");
+  };
+
+  const goToDiscover = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    history.push("/discover");
+  };
 
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className="h-dvh flex flex-col items-center justify-center">
-          <span className="mb-10">Je suis un </span>
-
-          <Button
-            color="primary"
-            variant="light"
-            onClick={(e) => {
-              e.preventDefault();
-              history.push("/stays");
-            }}
+          <div
+            className="c-pic c-pic-right absolute  h-full w-full"
+            style={{ backgroundImage: `url(${acteur})` }}
+            onClick={goToDiscover}
           >
-            Touriste
-          </Button>
-          <Button
-            color="danger"
-            variant="light"
-            onClick={(e) => {
-              e.preventDefault();
-              history.push("/discover");
-            }}
+            <div className="size-full bg-black bg-opacity-60 flex relative justify-center active:bg-opacity-0 transition-all">
+              <span className="absolute top-40 mx-auto text-5xl text-white">
+                Acteur
+              </span>
+            </div>
+          </div>
+          <div
+            className="c-pic c-pic-left absolute h-full w-full"
+            style={{ backgroundImage: `url(${touriste})` }}
+            onClick={goToStays}
           >
-            Acteur du secteur
-          </Button>
+            <div className="size-full bg-black bg-opacity-60 flex relative justify-center active:bg-opacity-0 transition-all">
+              <span className="absolute bottom-40 mx-auto text-5xl text-white">
+                Touriste
+              </span>
+            </div>
+          </div>
+          <div className="absolute flex justify-center items-center bg-white h-3 w-[800px] rotate-[24deg] blur-[4px]">
+            {/* <div className="bg-white rounded-full h-12 w-12 flex justify-center items-center">
+              OU
+            </div> */}
+          </div>
         </div>
       </IonContent>
     </IonPage>
